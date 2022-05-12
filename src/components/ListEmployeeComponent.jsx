a785cc234d5a722f40c2a53fcda019fee925a111
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-12 14:47:46 +0800

    First Commit

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import EmployeeService from "../services/EmployeeService";
+
+class ListEmployeeComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: [],
+    };
+    this.addEmployee = this.addEmployee.bind(this); // CreateEmployeeComponent
+  }
+
+  // Called immediately after a component is mounted.
+  // So this is the best place to call the rest api.
+  componentDidMount() {
+    EmployeeService.getEmployees().then((res) => {
+      this.setState({ employees: res.data });
+    });
+  }
+
+  // CreateEmployeeComponent
+  addEmployee() {
+    this.props.history.push("/add-employee");
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">Employees List</h2>
+        <div className="row">
+          <button className="btn btn-primary" onClick={this.addEmployee}>
+            {" "}
+            Add Employee
+          </button>
+        </div>
+        <div className="row">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Employee First Name</th>
+                <th>Employee Last Name</th>
+                <th>Employee Email Id</th>
+                <th>Action Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.employees.map((employee) => (
+                <tr key={employee.id}>
+                  <td>{employee.firstName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.emailId}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListEmployeeComponent;
