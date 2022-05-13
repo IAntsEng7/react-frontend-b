c6aaa8db8064794d4a712b84b36127049fc25545
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-13 11:25:35 +0800

    Header Footer and CSS

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 class HeaderComponent extends Component {
   constructor(props) {
     super(props);
-
     this.state = {};
   }
 
@@ -11,9 +10,9 @@ class HeaderComponent extends Component {
     return (
       <div>
         <header>
-          <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <nav className="navbar navbar-extend-md navbar-dark bg-dark">
             <div>
-              <a href="https://javaguides.net" className="navbar-brand">
+              <a href="http://localhost:3000/" className="navbar-brand">
                 Employee Management App
               </a>
             </div>
